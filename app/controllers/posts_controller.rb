44cc3358fe7be2ed200e4c14993f3ac6d7b9233f
julianche <julian.chernyk@gmail.com>
2015-07-07 22:34:08 +0200

    sorted posts and added author/user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,14 +6,19 @@ class PostsController < ApplicationController
 	def create
 		@forum = Forum.find(params[:forum_id])
 		@post = @forum.posts.build(post_params)
-		# @post.user = @user
-		@user = User.find(params[:user_id])
-		binding.pry
+		@post.user = current_user
+		# @user = User.find(params:[:user_id])
+		# @user = User.find(params[:user_id])
 		if @post.save 
 			redirect_to @forum
 		else 
 			render @post
 		end
+	end 
+
+	def show
+		@forum = Forum.find(params[:forum_id])
+		@post = @forum.posts.find(params[:id])
 	end 
 
 	private
