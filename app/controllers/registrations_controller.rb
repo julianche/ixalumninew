68ce72703e5a909784d6b2249ac780534449694d
Khangwelo <kmalotsha@gmail.com>
2015-07-07 22:32:31 +0200

    2nd Commit after Brian

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
-  def after_sign_in_path_for(user)
+  # def after_sign_in_path_for(user)
   	# if user.profile == true
   	# does user have a profile?
   	# if not, send to new_profile_path
