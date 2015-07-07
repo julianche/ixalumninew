4d07d4ee9b70daa23d83e700a8f8c031c3abf98b
Khangwelo <kmalotsha@gmail.com>
2015-07-07 17:42:09 +0200

    Added edit and update to controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -21,6 +21,19 @@ class ProfilesController < ApplicationController
 	    end
 	end
 
+	def edit 
+		@profile = Profile.find(params[:id])
+	end
+
+	def update
+		@profile = Profile.find(params[:id])
+		if @profile.update(profile_params)
+			redirect_to @profile
+		else
+			render "form"
+		end
+	end
+
 	private
 	def profile_params
 		params.require(:profile).permit(:occupation, :degree, :ixyear, :session, :university, :email, :city, :country )
