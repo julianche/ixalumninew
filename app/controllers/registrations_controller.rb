fc06d95b6b8e5d480b9633fa94adc152249d1590
Khangwelo <kmalotsha@gmail.com>
2015-07-07 19:56:13 +0200

    After Brians thing

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,16 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def after_sign_in_path_for(user)
+  	# does user have a profile?
+  	# if not, send to new_profile_path
+  	# otherwise, go to whatever
+  end
+
+  def after_sign_up_path_for(user)
+  	new_profile_path
+  end
+
   def sign_up_params
     params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
   end
