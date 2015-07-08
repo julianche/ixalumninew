1bb302773467659c3a1df229f204ffa7f7be2fb9
julianche <julian.chernyk@gmail.com>
2015-07-08 10:29:59 +0200

    worked on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
 	def create
 		@forum = Forum.find(params[:forum_id])
-		@post = @forum.posts.build(post_params)
+		@post = @forum.posts.build(params[:id])
 		@post.user = current_user
 		# @user = User.find(params:[:user_id])
 		# @user = User.find(params[:user_id])
@@ -21,14 +21,40 @@ class PostsController < ApplicationController
 		@post = @forum.posts.find(params[:id])
 	end 
 
+
+	def edit
+		@forum = Forum.find(params[:forum_id])
+		@post = @forum.posts.find(params[:id])
+		render 'edit'
+	end 
+
+
+	def index
+		@post = Post.all
+	end 
+
+	def update
+		@forum = Forum.find(params[:forum_id])
+		@post = @forum.posts.find(params[:id])
+		if @post.update(post_params)
+			redirect_to forum_post_path
+		else
+		render 'edit'
+		end 
+	end 
+
+	def destroy
+		@forum = Forum.find(params[:forum_id])
+		@post = @forum.posts.find(params[:id])
+		@post.destroy
+		redirect_to forum_path
+	end 
+	
+
 	private
 
-	def set_user
-		@user = current_user
-	end 
 
 	def post_params
 		params.require(:post).permit(:title, :body)
 	end 
-
 end 
--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,3 +1,3 @@
 class Forum < ActiveRecord::Base
-	has_many :posts
+	has_many :posts, dependent: :destroy
 end
