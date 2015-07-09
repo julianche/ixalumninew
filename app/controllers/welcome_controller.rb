c38a0b94f5258b702ae9946c321f2c4eabc0c697
julianche <julian.chernyk@gmail.com>
2015-07-09 11:43:42 +0200

    added bootstrap to homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,18 @@
 class WelcomeController < ApplicationController
+
+	def welcome
+		if current_user.profile == nil
+			redirect_to new_profile_path
+
+		else 
+			redirect_to home_path
+
+		end
+	end
+	
+	def index
+	end 
+
 	def home
 	end
 end
