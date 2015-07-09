ef5337fc6cb8b8cef90cb6d95ab08020cfbd5480
julianche <julian.chernyk@gmail.com>
2015-07-09 15:16:46 +0200

    fixed inability to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -52,11 +52,15 @@ ActiveRecord::Schema.define(version: 20150709123155) do
     t.string   "email"
     t.string   "city"
     t.string   "country"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
     t.string   "firstname"
     t.string   "lastname"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.string   "image_url"
   end
 
